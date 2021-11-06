68816a8770ff82d39422234cc8cd609974c12f34
luisluft <luberluft@gmail.com>
2021-11-06 17:52:17 -0400

    make the cart component openable and hidable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from "./components/Layout/Header";
-import React from "react";
+import React, { useState } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart></Cart>
-      <Header></Header>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -11,14 +11,16 @@ const Cart = () => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.54</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
