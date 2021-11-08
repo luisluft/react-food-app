fd375865e7de1b71edfabf4107853b5f612d8100
luisluft <luberluft@gmail.com>
2021-11-07 20:26:43 -0400

    update the badge on the cart icon when we add items to the cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
@@ -12,7 +24,9 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}></MealItemForm>
+        <MealItemForm
+          onAddToCart={addToCartHandler}
+          id={props.id}></MealItemForm>
       </div>
     </li>
   );
