ead8b7c20044159db1e6a53cb90a70a3aa9cb83d
luisluft <luberluft@gmail.com>
2021-11-18 08:35:45 -0400

    add checkout form input validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,29 +1,90 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postal: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = isNotEmpty(enteredName);
+    const enteredStreetIsValid = isNotEmpty(enteredStreet);
+    const enteredCityIsValid = isNotEmpty(enteredCity);
+    const enteredPostalIsValid = isFiveChars(enteredPostal);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postal: enteredPostalIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    //   submit the cart data
   };
+
+  const nameControlClass = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+  const streetControlClass = `${classes.control} ${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+  const cityControlClass = `${classes.control} ${
+    formInputsValidity.city ? "" : classes.invalid
+  }`;
+  const postalControlClass = `${classes.control} ${
+    formInputsValidity.postal ? "" : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClass}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
-      <div className={classes.control}>
+      {!formInputsValidity.name && <p>Please enter a valid name.</p>}
+      <div className={streetControlClass}>
         <label htmlFor="street">Your street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
-      <div className={classes.control}>
+      {!formInputsValidity.street && <p>Please enter a valid street.</p>}
+      <div className={postalControlClass}>
         <label htmlFor="postal">Your postal</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
-      <div className={classes.control}>
+      {!formInputsValidity.postal && <p>Please enter a valid postal.</p>}
+      <div className={cityControlClass}>
         <label htmlFor="city">Your city</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
+      {!formInputsValidity.city && <p>Please enter a valid city.</p>}
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
           Cancel
